809b82e37eadf6bed2e479e4e266fbce98778fa6
Rodolfo Pio Sassone <r.sassone3@studenti.uniba.it>
2021-11-01 19:42:44 +0100

    Aggiunto scheletro a client e server

--- clientTCP/src/clientTCP.c
+++ clientTCP/src/clientTCP.c
@@ -8,10 +8,46 @@
  ============================================================================
  */
 
+
+#if defined WIN32
+#include <winsock.h>
+#else
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#define closesocket close
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+#define NO_ERROR 0
+void clearwinsock() {
+#if defined WIN32
+	WSACleanup();
+#endif
 }
+
+int main(int argc, char *argv[]) {
+	//...
+#if defined WIN32
+	// Initialize Winsock
+	WSADATA wsa_data;
+	int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
+	if (result != NO_ERROR) {
+		printf("Error at WSAStartup()\n");
+		return 0;
+	}
+#endif
+	int my_socket;
+
+	//...
+
+	closesocket(my_socket);
+	clearwinsock();
+	return 0;
+} // main end
